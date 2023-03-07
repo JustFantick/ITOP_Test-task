34d85fde89606dc0462dabdba932e67b90400486
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-03-07 18:34:51 +0200

    Done convert component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import Header from './header/header.jsx';
 import Convert from './convert/convert.jsx';
 
 export default function App() {
-	const [amount1, setAmount1] = useState(0);
-	const [amount2, setAmount2] = useState(0);
+	const [amount1, setAmount1] = useState(10.0000);
+	const [amount2, setAmount2] = useState(9);
 
 	const [rates, setRates] = useState([]);
 
@@ -14,6 +14,8 @@ export default function App() {
 
 	const [currency1, setCurrency1] = useState('USD');
 	const [currency2, setCurrency2] = useState('EUR');
+
+
 
 	useEffect(() => {
 		let url = 'https://v6.exchangerate-api.com/v6/a5b5e34fb74721b636035bc6/latest/EUR';
@@ -25,12 +27,27 @@ export default function App() {
 	useEffect(() => {
 		setUsdToUan((1 * rates.UAH / rates.USD).toFixed(3));
 		setEurToUan((1 * rates.UAH / rates.EUR).toFixed(3));
-	}, [rates])
+		setAmount2((amount1 * rates[currency2] / rates[currency1]).toFixed(4));
+	}, [rates]);
+
+	// useEffect(() => {
+	// 	setAmount2((amount1 * rates[currency2] / rates[currency1]).toFixed(4));
+	// }, [currency1]);
+
+	// useEffect(() => {
+	// 	setAmount1((amount2 * rates[currency1] / rates[currency2]).toFixed(4));
+	// }, [currency2]);
 
 	return (
 		<div className="wrapper">
 			<Header usdToUan={usdToUan} eurToUan={eurToUan} />
-			<Convert />
+			<Convert
+				rates={rates}
+				amount1={amount1} setAmount1={setAmount1}
+				amount2={amount2} setAmount2={setAmount2}
+				currency1={currency1} setCurrency1={setCurrency1}
+				currency2={currency2} setCurrency2={setCurrency2}
+			/>
 		</div>
 	)
 }
--- src/components/convert/convert.jsx
+++ src/components/convert/convert.jsx
@@ -1,19 +1,135 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Convert() {
+export default function Convert(props) {
+
+	const [extraValue1, setExtraValue1] = useState('US Dollar, $1 = €0.9373');
+	const [extraValue2, setExtraValue2] = useState('Euro, €1 = ');
+
+	function arrowClickhandler(e) {
+		if (e.target.className !== 'input') {
+			e.target.closest('.convert-section').classList.toggle('active');
+		}
+	}
+
+	function onOptionClickHandler1(e) {
+		//unmark all options and mark the actual one
+		const options = e.target.closest('.convert-section__select-options').querySelectorAll('.convert-section__option');
+		options.forEach(option => {
+			option.classList.remove('selected');
+		});
+		e.target.classList.add('selected');
+
+		//actualize 'currency1' in parent`s state
+		let tempCurrency = '';
+		if (e.target.classList.contains('_usd')) {
+			props.setCurrency1('USD');
+			tempCurrency = 'USD';
+		} else if (e.target.classList.contains('_eur')) {
+			props.setCurrency1('EUR');
+			tempCurrency = 'EUR';
+		} else if (e.target.classList.contains('_uan')) {
+			props.setCurrency1('UAH');
+			tempCurrency = 'UAH';
+		}
+
+		//actualize 'amount2' when currency1 changes
+		props.setAmount2((props.amount1 * props.rates[props.currency2] / props.rates[tempCurrency]).toFixed(4));
+
+		//extra-info actualize
+		let currencyFullname =
+			tempCurrency === 'USD' ? 'US Dollar, $1 = ' :
+				tempCurrency === 'EUR' ? 'Euro, €1 = ' : 'Hryvnia, ₴1 = ';
+
+		let secondCurrencySymbol =
+			props.currency2 === 'USD' ? '$' :
+				props.currency2 === 'EUR' ? '€' : '₴';
+
+		setExtraValue1(currencyFullname + secondCurrencySymbol + (1 * props.rates[props.currency2] / props.rates[tempCurrency]).toFixed(4));
+	}
+
+	function onOptionClickHandler2(e) {
+		//unmark all options and mark the actual one
+		const options = e.target.closest('.convert-section__select-options').querySelectorAll('.convert-section__option');
+		options.forEach(option => {
+			option.classList.remove('selected');
+		});
+		e.target.classList.add('selected');
+
+		//actualize 'currency2' in parent`s state
+		let tempCurrency = '';
+		if (e.target.classList.contains('_usd')) {
+			props.setCurrency2('USD');
+			tempCurrency = 'USD';
+		} else if (e.target.classList.contains('_eur')) {
+			props.setCurrency2('EUR');
+			tempCurrency = 'EUR';
+		} else if (e.target.classList.contains('_uan')) {
+			props.setCurrency2('UAH');
+			tempCurrency = 'UAH';
+		}
+
+		//actualize 'amount1' when currency2 changes
+		props.setAmount1((props.amount2 * props.rates[props.currency1] / props.rates[tempCurrency]).toFixed(4));
+
+		//extra-info actualize
+		let currencyFullname =
+			tempCurrency === 'USD' ? 'US Dollar, $1 = ' :
+				tempCurrency === 'EUR' ? 'Euro, €1 = ' : 'Hryvnia, ₴1 = ';
+
+		let secondCurrencySymbol =
+			props.currency1 === 'USD' ? '$' :
+				props.currency1 === 'EUR' ? '€' : '₴';
+
+		setExtraValue2(currencyFullname + secondCurrencySymbol + (1 * props.rates[props.currency1] / props.rates[tempCurrency]).toFixed(4));
+	}
+
+	function onInputChangeHandler1(target) {
+		if (target.value !== '') {
+			props.setAmount1(parseFloat(target.value));
+			props.setAmount2((target.value * props.rates[props.currency2] / props.rates[props.currency1]).toFixed(4));
+
+			target.value = target.value.replace(/^0+/, '');
+		} else {
+			props.setAmount1(0);
+			props.setAmount2(0);
+		}
+	}
+
+	function onInputChangeHandler2(target) {
+		if (target.value !== '') {
+			props.setAmount2(parseFloat(target.value));
+			props.setAmount1((target.value * props.rates[props.currency1] / props.rates[props.currency2]).toFixed(4));
+
+			target.value = target.value.replace(/^0+/, '');
+		} else {
+			props.setAmount1(0);
+			props.setAmount2(0);
+		}
+	}
+
 	return (
 		<main className='convert'>
 			<div className="convert__body">
-				<div className="convert__section convert-section">
+				<div className="convert__section convert-section" onClick={(e) => arrowClickhandler(e)}>
 					<div className="convert-section__selected">
-						<div className="convert-section__extra-info">US Dollar, $1 = €0.903</div>
+						<div className="convert-section__extra-info">{extraValue1}</div>
 						<div className="convert-section__input">
-							<input type="number" name="amount1" id="amount1" />
+							<span>
+								{
+									props.currency1 === 'USD' ? '$' :
+										props.currency1 === 'EUR' ? '€' : '₴'
+								}
+							</span>
+							<input className='input' type="number" name="amount1" id="amount1"
+								value={props.amount1}
+								onChange={(e) => onInputChangeHandler1(e.target)} />
+							<img src={require('../../img/arrow-down.svg')} alt="arrows-down" />
 						</div>
 					</div>
-					<div className="convert-section__select-options">
-						<div className="convert-section__option">Euro €</div>
-						<div className="convert-section__option">Hryvnia ₴</div>
+					<div className="convert-section__select-options" onClick={(e) => onOptionClickHandler1(e)}>
+						<div className="convert-section__option selected _usd">US Dollar $</div>
+						<div className="convert-section__option _eur">Euro €</div>
+						<div className="convert-section__option _uan">Hryvnia ₴</div>
 
 					</div>
 
@@ -22,16 +138,27 @@ export default function Convert() {
 				<div className="convert__arrows">
 					<img src={require('../../img/arrows-icon.svg')} alt="arrows-icon" />
 				</div>
-				<div className="convert__section convert-section">
+
+				<div className="convert__section convert-section" onClick={(e) => arrowClickhandler(e)}>
 					<div className="convert-section__selected">
-						<div className="convert-section__extra-info">US Dollar, $1 = €0.903</div>
+						<div className="convert-section__extra-info">{extraValue2}</div>
 						<div className="convert-section__input">
-							<input type="number" name="amount1" id="amount1" />
+							<span>
+								{
+									props.currency2 === 'USD' ? '$' :
+										props.currency2 === 'EUR' ? '€' : '₴'
+								}
+							</span>
+							<input className='input' type="number" name="amount1" id="amount1"
+								value={props.amount2}
+								onChange={(e) => onInputChangeHandler2(e.target)} />
+							<img src={require('../../img/arrow-down.svg')} alt="arrows-down" />
 						</div>
 					</div>
-					<div className="convert-section__select-options">
-						<div className="convert-section__option">Euro €</div>
-						<div className="convert-section__option">Hryvnia ₴</div>
+					<div className="convert-section__select-options" onClick={(e) => onOptionClickHandler2(e)}>
+						<div className="convert-section__option _usd">US Dollar $</div>
+						<div className="convert-section__option _eur selected">Euro €</div>
+						<div className="convert-section__option _uan">Hryvnia ₴</div>
 
 					</div>
 
